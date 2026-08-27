6947f98b759b122fe1bdbd0510735d1494a6e922
Markus Domenegheti <markusvdc@gmail.com>
2026-08-27 08:14:55 -0300

    Exclude bosses from draconic damage

--- src/main/java/br/com/caphard/mixin/PlayerHardRulesMixin.java
+++ src/main/java/br/com/caphard/mixin/PlayerHardRulesMixin.java
@@ -2,6 +2,10 @@ package br.com.caphard.mixin;
 
 import br.com.caphard.gameplay.HardRules;
 import br.com.caphard.gameplay.HardRule;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import net.minecraft.world.entity.monster.warden.Warden;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -10,9 +14,16 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(Player.class)
 public abstract class PlayerHardRulesMixin {
 	@ModifyConstant(method = "hurtServer", constant = @Constant(floatValue = 3.0F))
-	private float caphard$applyHardDamage(float hardMultiplier) {
+	private float caphard$applyHardDamage(
+		float hardMultiplier,
+		ServerLevel level,
+		DamageSource source,
+		float damage
+	) {
 		Player player = (Player)(Object)this;
 		return HardRules.isActive(player.level(), HardRule.PLAYER_DAMAGE)
+			&& !(source.getEntity() instanceof Warden)
+			&& !(source.getEntity() instanceof EnderDragon)
 			? 4.0F
 			: hardMultiplier;
 	}
